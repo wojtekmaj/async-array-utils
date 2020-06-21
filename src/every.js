0d82b87b39e04ba0ceebf279dc46236990adcc87
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-06-21 22:02:39 +0200

    Remove unnecessary protection against calling resolve() multiple times

--- src/every.js
+++ src/every.js
@@ -2,19 +2,15 @@ import asyncForEach from './forEach';
 
 export default async function asyncSome(arr, fn) {
   return new Promise((resolve) => {
-    let resolved = false;
     // eslint-disable-next-line no-shadow
     asyncForEach(arr, async (cur, idx, arr) => {
       const result = await fn(cur, idx, arr);
 
       if (!result) {
-        resolved = true;
         resolve(false);
       }
     }).then(() => {
-      if (!resolved) {
-        resolve(true);
-      }
+      resolve(true);
     });
   });
 }
