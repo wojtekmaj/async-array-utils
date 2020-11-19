f93f7709f5002b0a0a6fc112cd088c3f3c37f545
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-11-19 12:36:13 +0100

    Minor formatting

--- src/reduce.js
+++ src/reduce.js
@@ -4,10 +4,11 @@ export default function asyncReduce(arr, fn, initialValue) {
   let temp = initialValue;
 
   return new Promise((resolve) => {
-    // eslint-disable-next-line no-shadow
     asyncForEachStrict(arr, (cur, idx) => new Promise((resolve2) => {
-      // eslint-disable-next-line no-await-in-loop
-      fn(temp, cur, idx).then((result) => { temp = result; resolve2(); });
+      fn(temp, cur, idx).then((result) => {
+        temp = result;
+        resolve2();
+      });
     }))
       .then(() => { resolve(temp); });
   });
