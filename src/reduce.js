67d0655b32c18aab7ad8dd8d0163ac3de454f4b9
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-11-19 12:28:42 +0100

    Refactor asyncReduce not to use async

--- src/reduce.js
+++ src/reduce.js
@@ -1,12 +1,14 @@
-export default async function asyncReduce(arr, fn, initialValue) {
+import asyncForEachStrict from './forEach_strict';
+
+export default function asyncReduce(arr, fn, initialValue) {
   let temp = initialValue;
 
-  for (let idx = 0; idx < arr.length; idx += 1) {
-    const cur = arr[idx];
-
-    // eslint-disable-next-line no-await-in-loop
-    temp = await fn(temp, cur, idx);
-  }
-
-  return temp;
+  return new Promise((resolve) => {
+    // eslint-disable-next-line no-shadow
+    asyncForEachStrict(arr, (cur, idx) => new Promise((resolve2) => {
+      // eslint-disable-next-line no-await-in-loop
+      fn(temp, cur, idx).then((result) => { temp = result; resolve2(); });
+    }))
+      .then(() => { resolve(temp); });
+  });
 }
